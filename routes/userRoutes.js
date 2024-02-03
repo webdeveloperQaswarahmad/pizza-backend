8fbc286925dfde2269dcad6c968dabed33447290
Qaswar664 <qaswarwebtechahmad@gmail.com>
2024-02-03 16:40:34 +0500

    added delte user and make user to admin route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -67,5 +67,74 @@ router.post("/register", async (req, res) => {
     }
   })
 
+// router.get("getallusers",async(req,res)=>{
+//   try {
+//     const users= await User.find({});
+//     res.status(200).send(users)
+
+//   } catch (error) {
+//     res.status(404).json({
+//       message:error.stack
+//     })
+//   }
+// })
+
+
+
+router.get('/getallusers', async (req, res) => {
+  try {
+    // Your logic to fetch users from the database
+    const users = await User.find({}).maxTimeMS(2000);
+    res.status(200).send(users);
+  } catch (error) {
+    res.status(404).json({
+      message: error.stack
+    });
+  }
+});
+
+// delete user account route 
+
+router.delete('/deleteuser/:userid', async (req, res) => {
+  const userid = req.params.userid;
+  try {
+    await User.findOneAndDelete({ _id: userid });
+    res.status(200).send('User deleted');
+  } catch (error) {
+    res.status(404).json({
+      message: error.stack,
+    });
+  }
+});
+
+//   make a user to admin route 
+router.put('/toggle-admin/:userId', async (req, res) => {
+  const userId = req.params.userId;
+  console.log('Received PUT request for userId:', userId);
+
+  try {
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    // Toggle the isAdmin status
+    user.isAdmin = !user.isAdmin;
+
+    // Save the updated user
+    await user.save();
+
+    // Return the updated user data
+    return res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+
+
+
+
 
 module.exports = router;
